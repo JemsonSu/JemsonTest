7cf1ccc8693b8f61a1ade6aebea7b7a65463ca20
Jemson <suzhixian@dingtalk.com>
2019-09-19 16:02:55 +0800

    第一次提交

---
+++ bigdata/src/main/java/com/jemson/Test.java
@@ -0,0 +1,7 @@
+package com.jemson;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试大数据...");
+    }
+}
---
+++ bigdata/src/main/java/com/jemson/hadoop/MyWC.java
@@ -0,0 +1,10 @@
+package com.jemson.hadoop;
+
+/**
+ * 这是一个hadoop wordcout程序
+ */
+public class MyWC {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
