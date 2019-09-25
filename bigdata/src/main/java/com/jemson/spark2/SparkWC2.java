7dae4b9259f8e04a7306a6c27fddf39f686a0e02
Jemson <suzhixian@dingtalk.com>
2019-09-25 09:31:04 +0800

    更新java spark wordcount程序

--- bigdata/src/main/java/com/jemson/spark2/SparkWC2.java
+++ bigdata/src/main/java/com/jemson/spark2/SparkWC2.java
@@ -1,11 +1,11 @@
 package com.jemson.spark2;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.*;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
@@ -18,22 +18,28 @@ import java.util.List;
 /**
  * spark 2.X 的wordcount 程序
  */
-public class SparkWC {
+public class SparkWC2 {
     public static void main(String[] args) {
         System.out.println("spark2.4.4 wordcount 测试");
 
-        String logFile = "./bigdata/a.txt";
-        //String logFile = args[0];
+        //String logFile = "./bigdata/a.txt";
+        String logFile = args[0];
         System.out.println("计算文件:"+logFile);
 
-        SparkConf conf = new SparkConf();
-        conf.setAppName("SparkWC");
-        conf.setMaster("local");
-        JavaSparkContext jsc = new JavaSparkContext(conf);
-        JavaRDD<String> linesRDD = jsc.textFile(logFile);
+        SparkSession.Builder builder = SparkSession.builder();
+        //builder.appName("spark2.4.4 wordcount");
+        //builder.master("yarn");
+        SparkSession sparkSession = builder.getOrCreate();
+        //sparkSession.sparkContext().setLogLevel("ERROR"); //只输出ERROR级别日志
+
+        DataFrameReader read = sparkSession.read();
+
+        Dataset<String> dataset = read.textFile(logFile);
 
 
-        JavaRDD<String> worksRDD = linesRDD.flatMap(new FlatMapFunction<String, String>() {
+        JavaRDD<String> linesRDD = dataset.javaRDD(); //Dataset转javaRDD
+
+        JavaRDD<String> rdd2 = linesRDD.flatMap(new FlatMapFunction<String, String>() {
             public Iterator<String> call(String line) throws Exception {
                 System.out.println("line=" + line);
                 String[] words = line.split(" ");
@@ -42,14 +48,14 @@ public class SparkWC {
             }
         });
 
-        JavaPairRDD<String, Long> onesRDD = worksRDD.mapToPair(new PairFunction<String, String, Long>() {
+        JavaPairRDD<String, Long> rdd3 = rdd2.mapToPair(new PairFunction<String, String, Long>() {
             public Tuple2<String, Long> call(String work) throws Exception {
                 System.out.println("work:"+work);
                 return new Tuple2<String, Long>(work, 1L);
             }
         });
 
-        JavaPairRDD<String, Long> countsRDD = onesRDD.reduceByKey(new Function2<Long, Long, Long>() {
+        JavaPairRDD<String, Long> rdd4 = rdd3.reduceByKey(new Function2<Long, Long, Long>() {
             public Long call(Long v1, Long v2) throws Exception {
                 System.out.println(v1+"--"+v2);
                 return v1 + v2;
@@ -57,16 +63,16 @@ public class SparkWC {
         });
 
 
-        countsRDD.foreach(new VoidFunction<Tuple2<String, Long>>() {
+        rdd4.foreach(new VoidFunction<Tuple2<String, Long>>() {
             public void call(Tuple2<String, Long> tuple2) throws Exception {
-                System.out.println(tuple2._1 + "=" + tuple2._2());
+                System.out.println(tuple2._1 + "==" + tuple2._2());
             }
         });
 
 
 
         //关闭资源，close方法也是调用stop方法
-        jsc.stop();
+        sparkSession.stop();
 
     }
 
